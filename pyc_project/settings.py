85829d0891366fc44b52bf673bb745048eca5c9c
Caitlin Snyder <caitsnyder@gmail.com>
2019-11-11 12:48:10 -0500

    allowed hosts typo2'

--- pyc_project/settings.py
+++ pyc_project/settings.py
@@ -11,7 +11,7 @@ os.environ["SECRET_KEY"]
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com’]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
