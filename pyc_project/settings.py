e9d7edf22b4d6164cb17351c213d1c56d9374c24
Caitlin Snyder <caitsnyder@gmail.com>
2019-11-11 13:04:17 -0500

    with secret key declared

--- pyc_project/settings.py
+++ pyc_project/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
