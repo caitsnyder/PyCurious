1a1e8d2583732637e5aaaee377f540dc927c29f0
Caitlin Snyder <caitsnyder@gmail.com>
2019-11-11 17:49:03 -0500

    with staticfiles resolved

--- pyc_project/settings.py
+++ pyc_project/settings.py
@@ -119,17 +119,17 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'sass_processor.finders.CssFinder',
 ]
 
+# Django Sass
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static')
+
+
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Django Sass
-SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR,'static/scss')
 
 # Configure Django App for Heroku.
 import django_heroku
