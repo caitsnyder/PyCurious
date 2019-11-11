4fa6637e79ee007b04e418e827e138bc78c81198
Caitlin Snyder <caitsnyder@gmail.com>
2019-11-11 16:55:22 -0500

    updated timedate for post model

--- pyc/models.py
+++ pyc/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=200)
 	author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
 	body = models.TextField()
-	date = models.DateTimeField(default=datetime.now)
+	date = timezone.now()
 
 	def __str__(self):
 		return self.title
