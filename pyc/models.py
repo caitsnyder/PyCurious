712edd25d248248ad16ac1715fee2089728d2b49
Caitlin Snyder <caitsnyder@gmail.com>
2019-11-12 18:08:29 -0500

    with stylistic changes

--- pyc/models.py
+++ pyc/models.py
@@ -7,9 +7,10 @@ class Post(models.Model):
 	title = models.CharField(max_length=200)
 	author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
 	body = models.TextField()
-	date = timezone.now()
+	timestamp = models.DateTimeField(default=timezone.now)
+
+	class Meta:
+		ordering = ['-timestamp']
 
 	def __str__(self):
 		return self.title
-
-		
--- pyc/urls.py
+++ pyc/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from .views import PycListView, PycDetailView, PycResumeView
 
 urlpatterns = [
-	path('post/<int:pk>/', 
-		PycDetailView.as_view(), 
-		name='post_detail'),
+	path('post/<int:pk>/', PycDetailView.as_view(), name='post_detail'),
 	path('', PycListView.as_view(), name='home'),
 	path('resume', PycResumeView.as_view(), name='resume'),
 ]
